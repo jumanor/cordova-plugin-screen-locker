bf01f7264a85e9873c739170397de28224651c4b
jumanor <jumanor@jumanors-iMac.local>
2020-03-14 15:48:55 -0500

    add  >8.1

--- src/android/ScreenLocker.java
+++ src/android/ScreenLocker.java
@@ -22,6 +22,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.annotation.SuppressLint;
+import android.os.Build;
+
 public class ScreenLocker extends CordovaPlugin {
     public static final String TAG = "ScreenLocker";
     public static final String ACTION_UNLOCK = "unlock";
@@ -42,6 +45,8 @@ public class ScreenLocker extends CordovaPlugin {
      * @param cordova The context of the main Activity.
      * @param webView The CordovaWebView Cordova is running in.
      */
+    
+    @SuppressLint("InvalidWakeLockTag")
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
 
@@ -83,23 +88,47 @@ public class ScreenLocker extends CordovaPlugin {
                 cordova.getActivity().runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        Window window = cordova.getActivity().getWindow();
-                    	window.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
-                    	window.addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
-                    	window.addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
 
-                        if(wakeLock.isHeld()) {
-                            wakeLock.release();
+                            cordova.getActivity().setShowWhenLocked(true);
+                            cordova.getActivity().setTurnScreenOn(true);
+
+                            KeyguardManager keyguardManager = (KeyguardManager) cordova.getActivity().getSystemService(Context.KEYGUARD_SERVICE);
+                            keyguardManager.requestDismissKeyguard(cordova.getActivity(), null);
+
+                            if(wakeLock.isHeld()) {
+                                wakeLock.release();
+                            }
+                            if(acquireTime > 0) {
+                                wakeLock.acquire(acquireTime);
+                            }
+                            else
+                            {
+                                wakeLock.acquire();
+                            }
+                            Log.v(TAG, "ScreenLocker received SUCCESS:" + action);
+                            callbackContext.success();
                         }
-                        if(acquireTime > 0) {
-                            wakeLock.acquire(acquireTime);
+                        else{
+
+                            Window window = cordova.getActivity().getWindow();
+                            window.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+                            window.addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+                            window.addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+
+                            if(wakeLock.isHeld()) {
+                                wakeLock.release();
+                            }
+                            if(acquireTime > 0) {
+                                wakeLock.acquire(acquireTime);
+                            }
+                            else
+                            {
+                                wakeLock.acquire();
+                            }
+                            Log.v(TAG, "ScreenLocker received SUCCESS:" + action);
+                            callbackContext.success();
                         }
-                        else
-                        {
-                            wakeLock.acquire();
-                        }
-                        Log.v(TAG, "ScreenLocker received SUCCESS:" + action);
-                        callbackContext.success();
                     }
                 });
 
